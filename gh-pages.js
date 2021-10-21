d250a647375abff098dc829b08a8b67b86d0f963
Andrian <andrian.kozhomin@zade.agency>
2021-10-21 17:07:31 +0300

    initial gh-pages commit

--- gh-pages.js
+++ gh-pages.js
@@ -6,8 +6,8 @@ ghpages.publish(
     branch: 'gh-pages',
     repo: 'https://github.com/Kampel6263/pollList-app', // Update to point to your repository  
     user: {
-      name: 'Your name', // update to use your name
-      email: 'Your Email address' // Update to use your email
+      name: 'Kampel6263', // update to use your name
+      email: 'andrian6263@gmail.com' // Update to use your email
     }
   },
   () => {
